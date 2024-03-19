412df42893cb3d14e891c794e55ee860ef0c30a9
praveenkumar09 <tpkprav@gmail.com>
2024-03-19 19:42:05 +0800

    basic add, subract.multiply, divide fn added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,41 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteDesc(operator,resultbeforeCalc, calcNumber){
+    const calcDesc = `${resultbeforeCalc} ${operator} ${calcNumber}`;
+    return calcDesc;
+}
+
+
 function add(){
-    const result =  currentResult + parseInt(userInput.value);
-    outputResult(result,currentResult + " + "+ userInput.value);
+    const userInputValue = getUserInput();
+    const calcResult =  currentResult + userInputValue;
+    outputResult(calcResult,createAndWriteDesc("+",currentResult,userInputValue));
+}
+
+function subract(){
+    const userInputValue = getUserInput();
+    const calcResult =  currentResult - userInputValue;
+    outputResult(calcResult,createAndWriteDesc("-",currentResult,userInputValue));
+}
+
+function multiply(){
+    const userInputValue = getUserInput();
+    const calcResult =  currentResult * userInputValue;
+    outputResult(calcResult,createAndWriteDesc("*",currentResult,userInputValue));
+}
+
+function divide(){
+    const userInputValue = getUserInput();
+    const calcResult =  currentResult / userInputValue;
+    outputResult(calcResult,createAndWriteDesc("/",currentResult,userInputValue));
 }
 
 addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
